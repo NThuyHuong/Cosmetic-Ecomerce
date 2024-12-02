e4cfafcfe048acaf4656f5473f8242d75e4a4fba
NThuyHuong <nguyenhuongyoi15@gmail.com>
2024-12-02 10:26:52 +0700

    init project

---
+++ Ashion/Migrations/202403251330303_DatabaseNew.cs
@@ -0,0 +1,20 @@
+﻿namespace Ashion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DatabaseNew : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Order", "Status", c => c.Int(nullable: false));
+            AlterColumn("dbo.tb_Order", "TotalAmount", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.tb_Order", "TotalAmount", c => c.String());
+            DropColumn("dbo.tb_Order", "Status");
+        }
+    }
+}
